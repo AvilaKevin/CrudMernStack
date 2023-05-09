88b66ac85a3cf59cf7f107b475fb1403d18599a1
ViperDex <viperdex2000@hotmail.com>
2023-05-08 20:12:42 -0500

    create.css create.jsx  completed

--- crud-ui/src/pages/Create.jsx
+++ crud-ui/src/pages/Create.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import Styles from '../css/pagesCss/Create.module.css';
+import { BsPhone } from 'react-icons/bs';
+import { HiOutlineMail } from 'react-icons/hi';
+
 
 function Create() {
     return (
-        <div>Create</div>
-    )
-}
+        <div className={Styles.Container}>
 
-export default Create
+            <div className={Styles.FormContainer}>
+
+                <h1 className={Styles.h1}>Create Customer</h1>
+
+                <form className={Styles.Form}>
+
+                    <label for="name" className={Styles.Label}>Name</label>
+                    <input type="text" id="name" name="name" placeholder='Kevin' className={Styles.InputWhitoutIcon} />
+
+                    <label for="email" className={Styles.Label}>E-mail</label>
+                    <div>
+                        <i className={Styles.icon}>
+                            <HiOutlineMail />
+                        </i>
+                        <input type="email" id="email" name="email" placeholder='example@gmail.com' className={Styles.Input} />
+                    </div>
+
+                    <label for="phone" className={Styles.Label}>Phone Number</label>
+                    <div>
+                        <i className={Styles.icon}>
+                            <BsPhone />
+                        </i>
+                        <input type="tel" id="phone" name="phone" placeholder='3632941994' class={Styles.Input} />
+                    </div>
+
+                    <button className={Styles.CreateButton}>
+                        Create
+                    </button>
+                    <button className={Styles.DiscardButton} >
+                        Discard
+                    </button>
+
+                </form>
+
+            </div>
+
+        </div>
+    )
+};
+
+export default Create;

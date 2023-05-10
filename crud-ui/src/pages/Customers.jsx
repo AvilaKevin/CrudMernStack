3075d94fe0c628d464df2161cb4f33ceb3baa24c
ViperDex <viperdex2000@hotmail.com>
2023-05-10 16:19:09 -0500

    creation of  errComponent

--- crud-ui/src/pages/Customers.jsx
+++ crud-ui/src/pages/Customers.jsx
@@ -4,11 +4,14 @@ import { RiSearchLine } from 'react-icons/ri'
 import User from '../components/User'
 import { useState } from 'react'
 import axios from 'axios'
+import { AiOutlineFileExcel } from 'react-icons/ai'
 
 function Dashboard() {
 
     // Se crea un estado que almacena la informacion de la api
     const [customers, setCustomers] = useState([]);
+
+    const [input, setInput] = useState("");
 
     // Se hace uso de useEffect para captuarar la informacion de la api antes de que se renderice el componente
     useEffect(() => {
@@ -26,6 +29,11 @@ function Dashboard() {
         fetchCustomers();
     }, []);
 
+    const handleSearch = async () => {
+        const res = await axios.get(`/search?q=${input}`)
+        setCustomers(res.data)
+    }
+
     return (
 
         <div className={Styles.Container}>
@@ -34,16 +42,20 @@ function Dashboard() {
 
             <div className={Styles.SearchContainer}>
 
-                <input className={Styles.Input} placeholder="Search..." />
-                <i><RiSearchLine /></i>
+                <input className={Styles.Input} placeholder="Search..." onChange={(e) => setInput(e.target.value)} />
+                <i onClick={handleSearch}><RiSearchLine /></i>
 
             </div>
 
             {/* se hace uso de map para renderizar cada uno de los elementos que se traen de la api, 
             es necesario agregar un identificador osea key para que react no genere error, este puede ser el id, se envia la informacio capturada de la api a nuestro componente User para mostrar correctamente la informacion */}
-            {customers.map((customer) => (
-                <User key={customer._id} customer={customer} />
-            ))}
+            {customers.length === 0 ? <div className={Styles.ErrContainer}>
+                <h1 className={Styles.h1}>Ups, no data</h1>
+                <i className={Styles.ErrIcon}><AiOutlineFileExcel /></i>
+            </div> :
+                customers.map((customer) => (
+                    <User key={customer._id} customer={customer} />
+                ))}
 
         </div>
 
